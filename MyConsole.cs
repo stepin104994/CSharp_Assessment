0c21a29453681e4e6fa00afe6c2b661ea5bc803a
Nithya <69047860+stepin104994@users.noreply.github.com>
2020-10-03 14:43:54 +0530

    Add files via upload

---
+++ MyConsole.cs
@@ -0,0 +1,25 @@
+﻿ using System;
+ namespace SampleConApp
+{    
+        class MyConsole
+         {            
+             internal static double getDouble(string message)
+             {
+                 Console.WriteLine(message);
+                 return double.Parse(Console.ReadLine());
+             }
+
+            internal static string getString(string message)
+             {
+                 Console.WriteLine(message);
+                 return Console.ReadLine();
+             }
+
+             internal static int getNumber(string message)
+             {
+                 return int.Parse(getString(message));
+             }            
+
+         }
+
+ }
---
+++ Serialization.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+namespace SampleConApp
+{
+    [Serializable]
+    public class Employee
+    {
+        public string EmpName { get; set; }
+        public string EmpAddress { get; set; }
+        public long EmpPhone { get; set; }
+
+    }
+    class Serialization
+    {
+        static void Main()
+        {
+            binaryWorking();            
+            Console.ReadKey();
+        }
+        private static void binaryWorking()
+        {
+            Console.WriteLine("Your wish to proceed: WRITE or READ");
+            string choice = Console.ReadLine();
+            if (choice.ToLower() == "read")
+                deserializingBinary();
+            else
+                serializingBinary();
+
+        }
+        private static void deserializingBinary()
+        {
+            try
+            {
+                FileStream fileStr = new FileStream("Details.bin", FileMode.Open, FileAccess.Read);
+                BinaryFormatter binF = new BinaryFormatter();
+                Employee emp = binF.Deserialize(fileStr) as Employee;
+                Console.WriteLine($"Name: {emp.EmpName}\nAddress: {emp.EmpAddress}\nPhone Number: {emp.EmpPhone}");
+                fileStr.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No file available to read..Kindly write(create) file before reading.....Thank you :)....");
+            }
+        }
+
+        private static void serializingBinary()
+        {
+
+            Employee emp = new Employee { EmpName = "NITHYA P", EmpAddress = "OOTY",  EmpPhone = 24312345 };
+            BinaryFormatter binF = new BinaryFormatter();
+            FileStream fileStr = new FileStream("Details.bin", FileMode.OpenOrCreate, FileAccess.Write);
+            binF.Serialize(fileStr, emp);
+            fileStr.Close();
+        }  
+       
+    }
+}
